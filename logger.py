dea361afd28170f3b013d4b40dc894124b1b42e0
RiWieland <61945371+wielandos@users.noreply.github.com>
2023-06-23 20:47:50 +0200

    add init for logger.py

--- logger.py
+++ logger.py
@@ -5,3 +5,9 @@ class Loggers_Base:
     '''
     Logging Class for initialize the logging into Azure Application Insights
     '''
+    def initialize_logger(self, name=None):
+        logging.basicConfig(level=logging.INFO)
+        logger_.setLevel('INFO')
+        conn_str = (f'InstrumentationKey={os.environ["APPINSIGHTS_INSTRUMENTATIONKEY"]}')
+        logger_: logging.Logger = logger_
+        return logger_
