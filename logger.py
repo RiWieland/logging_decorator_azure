7ca01f62c43b0a5475fee63c31093cf0f66eb9ef
RiWieland <61945371+wielandos@users.noreply.github.com>
2023-07-05 14:16:37 +0200

    added try and except

--- logger.py
+++ logger.py
@@ -28,14 +28,19 @@ class Logger_Decorators:
     '''
     def __init__(self, logger_):
         self.logger_ = logger_
-       
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            args_repr = [repr(a) for a in args]
-            kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
-            signature = ", ".join(args_repr + kwargs_repr)
-            self.logger_.info(f"function {func.__name__} called with args {signature}")
-            result = func(*args, **kwargs)
-            return result
-        return decorator
+
+    def send_logs(self):
+        def decorator(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                try:
+                    args_repr = [repr(a) for a in args]
+                    kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+                    signature = ", ".join(args_repr + kwargs_repr)
+                    self.logger_.info(f"function {func.__name__} called with args {signature}")
+                    result = func(*args, **kwargs)
+                    return result
+                except Exception as e:
+                    self.logger_.exception(f"Exception raised in function {func.__name__}. Exception: {str(e)}")
+                    self.logger_.warning(f"function {func.__name__} called with args {signature} and thrown an error {str(e)}")
+            return decorator
